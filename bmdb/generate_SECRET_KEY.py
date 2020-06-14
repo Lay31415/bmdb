7d6f6317dbd04e06032127b754c88c26fd7779c1
Lay31415 <lay31415@gmail.com>
2020-06-14 20:04:46 +0900

    Initialize Django!

---
+++ bmdb/generate_SECRET_KEY.py
@@ -0,0 +1,7 @@
+# Please run first `python generate_secretkey_setting.py > local_settings.py`
+
+from django.core.management.utils import get_random_secret_key
+
+secret_key = get_random_secret_key()
+text = 'SECRET_KEY = \'{0}\''.format(secret_key)
+print(text)
